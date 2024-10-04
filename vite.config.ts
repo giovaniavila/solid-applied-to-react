81565431d32ec4918d0a07cd3aa4a7c88b7a8692
Gio <giovanicarvalhoavila@hotmail.com>
2024-10-04 10:53:58 -0300

    fix: add gh-pages dependecies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/solid-applied-to-react/', // Ajuste aqui para o nome do seu repositório
+});
