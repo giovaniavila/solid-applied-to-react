9e0e1767ae9a7fed3341a186bcd3a8051a2d810c
Gio <giovanicarvalhoavila@hotmail.com>
2024-10-11 16:30:24 -0300

    fix: change the liskov principle example

--- src/priciples/liskov/example.tsx
+++ src/priciples/liskov/example.tsx
@@ -1,50 +1,12 @@
-export const codeSnippet = `// Base class
-class Shape {
-    area(): number {
-        throw new Error("Method not implemented.");
-    }
-}
+export const codeSnippet = `
+// base component
+const Button = ({ onClick, label }: { onClick: () => void; label: string }) => (
+  <button onClick={onClick}>{label}</button>
+);
 
-// Subclass for Circle
-class Circle extends Shape {
-    constructor(private radius: number) {
-        super();
-    }
+// derived component
+const PrimaryButton = (props: { onClick: () => void; label: string }) => (
+  <Button {...props} style={{ backgroundColor: "blue", color: "white" }} />
+);
 
-    area(): number {
-        return Math.PI * this.radius * this.radius;
-    }
-}
-
-// Subclass for Rectangle
-class Rectangle extends Shape {
-    constructor(private width: number, private height: number) {
-        super();
-    }
-
-    area(): number {
-        return this.width * this.height;
-    }
-}
-
-// Function to calculate the area of a shape
-const calculateArea = (shape: Shape): number => {
-    return shape.area();
-};
-
-// Main Component
-const MyComponent: React.FC = () => {
-    const [shape, setShape] = useState<Shape>(new Circle(5)); // Default shape is a Circle
-
-    const handleSwitchShape = () => {
-        setShape(new Rectangle(4, 6)); // Substitute with a Rectangle
-    };
-
-    return (
-        <div>
-            <h1>Area: {calculateArea(shape)}</h1>
-            <button onClick={handleSwitchShape}>Change to Rectangle</button>
-        </div>
-    );
-};
 `;
